a3a3a04d0892012ac568d74642041afb8f88ce39
DonDobhran <87570856+DonDobhran@users.noreply.github.com>
2021-07-26 02:00:55 +0500

    Delete DZ1_Zadanie_6.py

--- DZ1_Zadanie_6.py
+++
@@ -1,8 +0,0 @@
-result_1day = int(input("Результат спортсмена в первый день: "))
-gelanie = int(input("Желаемая дистанция"))
-days = 1
-while result_1day < gelanie:
-    result_1day *= 1.1
-    days += 1
-
-print(f"Спортсмен достигнет дистанции в {gelanie} км за {days} дней")
